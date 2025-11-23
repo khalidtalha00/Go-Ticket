cfd610b5f7f19a41747b8f71c5c2d44328cdecac
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2025-11-23 19:39:19 +0530

    secondary

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,13 +14,17 @@ import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
 import image4 from '../assets/image4.png';
 import image5 from '../assets/image5.jpg';
+import image6 from '../assets/image6.webp';
+import image7 from '../assets/image7.jpg';
 
 const galleryImages = [
   image1,
   image2,
   image3,
   image4,
-  image5
+  image5,
+  image6,
+  image7
 ];
 
 const features = [
