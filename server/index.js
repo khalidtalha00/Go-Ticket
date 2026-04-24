6f23bbf0032d59337ef845e985a5a410e935d9b5
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2026-04-24 19:15:19 +0530

    Add 'https://go-ticket123.vercel.app' to allowed CORS origins

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   process.env.FRONTEND_URL_2,
+  'https://go-ticket123.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
 ].filter(Boolean);
